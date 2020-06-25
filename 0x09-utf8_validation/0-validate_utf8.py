74c730c543bdb992548d86ea6336009178e9059a
alejogonza <alejo.1996.2001@gmail.com>
2020-06-24 23:41:35 -0500

    [fix] datas

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
 
-
 def validUTF8(data):
-    """Determines if given data represents valid UTF-8 encoding
-
-    Args:
-        data: list of integers
-
-    Returns:
-        True if valid UTF-8 encoding, otherwise False
-    """
-    bit_count = 0
-    for n in data:
-        mask = 1 << 7
-        if not bit_count:
-            while n & mask:
-                bit_count += 1
-                mask >>= 1
-            if bit_count > 4:
-                return False
-        elif n >> 6 != 2:
-            return False
-        if bit_count:
-            bit_count -= 1
-    return bit_count == 0
+        """
+        A character in UTF-8 can be 1 to 4 bytes long
+        The data set can contain multiple characters
+        The data will be represented by a list of integers
+        Each integer represents 1 byte of data, therefore you only need to handle the 8 least significant bits of each integer
+        data = [65]
+        True
+        data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
+        True
+        data = [229, 65, 127, 256]
+        False
+        """
+        successive_10 = 0
+        for b in data:
+                b = bin(b).replace('0b','').rjust(8, '0')
+                if successive_10 != 0:
+                        successive_10 -= 1
+                        if not b.startswith('10'):
+                                return False
+                elif b[0] == '1':
+                        successive_10 = len(b.split('0')[0]) - 1
+        return True
