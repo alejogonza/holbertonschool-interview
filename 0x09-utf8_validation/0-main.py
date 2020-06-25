37db23f2b0204d3b2f39ac016464f69e3c833016
alejogonza <alejo.1996.2001@gmail.com>
2020-06-24 23:45:33 -0500

    [fix] datas

--- 0x09-utf8_validation/0-main.py
+++ 0x09-utf8_validation/0-main.py
@@ -6,16 +6,10 @@ Main file for testing
 validUTF8 = __import__('0-validate_utf8').validUTF8
 
 data = [65]
-print(validUTF8(data))                                                          # True
-
-data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]   # True
 print(validUTF8(data))
 
-data = [229, 65, 127, 256]                                                      # False
+data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
 print(validUTF8(data))
 
-data = [197, 130, 1]                                                            # True
-print(validUTF8(data))
-
-data = [235, 140, 4]                                                            # False
+data = [229, 65, 127, 256]
 print(validUTF8(data))
--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,25 +1,23 @@
-#!/usr/bin/python3
+#!/bin/usr/python3
+""" Module for validating utf-8
+"""
+
+import sys
+
 
 def validUTF8(data):
-        """
-        A character in UTF-8 can be 1 to 4 bytes long
-        The data set can contain multiple characters
-        The data will be represented by a list of integers
-        Each integer represents 1 byte of data, therefore you only need to handle the 8 least significant bits of each integer
-        data = [65]
-        True
-        data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
-        True
-        data = [229, 65, 127, 256]
-        False
-        """
-        successive_10 = 0
-        for b in data:
-                b = bin(b).replace('0b','').rjust(8, '0')
-                if successive_10 != 0:
-                        successive_10 -= 1
-                        if not b.startswith('10'):
-                                return False
-                elif b[0] == '1':
-                        successive_10 = len(b.split('0')[0]) - 1
-        return True
+    """ Method for validating utf-8
+    @param data: data to test
+    @return: True if utf-8 else false
+    """
+    i = 0
+    while i < len(data):
+        if data[i] >> 7 & 0:
+            i += 1
+            continue
+
+    return True
+
+
+def validUTF8_bytes(data, n):
+    """ method that will validate a set of bytes as utf-8"""
